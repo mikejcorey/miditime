4bd1b33843329a314221376a124976dc632de131
Michael Corey <mikejcorey@gmail.com>
2015-05-12 15:26:09 -0700

    Messing with midiutil path

--- setup.py
+++ setup.py
@@ -78,7 +78,8 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['midiutil'],
+    # install_requires=['midiutil'],
+    install_requires=['git+git://github.com/duggan/midiutil'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
