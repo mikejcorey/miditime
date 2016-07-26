412404ad189a155bcf388c99dc848fac9b9f51bb
Michael Corey <mikejcorey@gmail.com>
2016-07-25 21:06:21 -0700

    version up.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.7',
+    version='1.0.8',
 
     description='Generate MIDI files from time series data. You can control can control what octaves and octave ranges you want.',
     # long_description=long_description,
