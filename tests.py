57070f740efe00fa00ad58f15186747af0ae917b
Michael Corey <corey101@umn.edu>
2024-10-13 11:33:39 -0500

    added test of example

--- tests.py
+++ tests.py
@@ -54,6 +54,26 @@ class OctaveTest(unittest.TestCase):
         self.assertEqual(m.scale_to_note(pct, ['D', 'E', 'F', 'G', 'A', 'Bb', 'C']), 'Bb7')
 
 
+class DemoTest(unittest.TestCase):
+    def test_basic_example(self):
+        # from miditime.miditime import MIDITime
+
+        # Instantiate the class with a tempo (120bpm is the default) and an output file destination.
+        mymidi = MIDITime(120, 'myfile.mid')
+
+        # Create a list of notes. Each note is a list: [time, pitch, velocity, duration]
+        midinotes = [
+            [0, 60, 127, 3],  #At 0 beats (the start), Middle C with velocity 127, for 3 beats
+            [10, 61, 127, 4]  #At 10 beats (12 seconds from start), C#5 with velocity 127, for 4 beats
+        ]
+
+        # Add a track with those notes
+        mymidi.add_track(midinotes)
+
+        # Output the .mid file
+        mymidi.save_midi()
+
+
 def main():
     unittest.main()
 
